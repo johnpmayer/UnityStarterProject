a7add6c8141fac5bd8e2e9999a57d062a2e5511a
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2018-07-06 18:37:46 -0400

    ship piloting throttle controls. very funny boarded-player spring movement!

--- workers/unity/Assets/Gamelogic/Ship/PilotMovement.cs
+++ workers/unity/Assets/Gamelogic/Ship/PilotMovement.cs
@@ -14,8 +14,49 @@ namespace Assets.Gamelogic.Ship
     {
         [Require] private Position.Writer PositionWriter;
 
+        private float _targetThrotttle;
+        private float _targetRudder;
+
+        private Rigidbody rb;
+
+        private void OnEnable()
+        {
+            rb = gameObject.GetComponent<Rigidbody>();
+        }
+
         public void SetControls(PilotControls.Update update) {
+            if (update.rudder.HasValue) {
+                _targetRudder = update.rudder.Value;
+            }
+            if (update.propellor.HasValue) {
+                _targetThrotttle = update.propellor.Value;
+            }
             Debug.LogWarning("TODO: Setting controls on ship behavior");
+        }
+
+        public void FixedUpdate()
+        {
+            var force = new Vector3(_targetThrotttle, 0.0f, 0.0f);
+            var position = new Vector3(0.0f, 0.0f, 0.0f);
+            rb.AddForceAtPosition(force, position);
+        }
+
+        public void Update()
+        {
+            PositionWriter.Send(new Position.Update().SetCoords(gameObject.transform.position.ToCoordinates()));
+        }
+    }
+
+    public static class Vector3Extensions
+    {
+        public static Coordinates ToCoordinates(this Vector3 vector3)
+        {
+            return new Coordinates(vector3.x, vector3.y, vector3.z);
+        }
+
+        public static Vector3f ToNativeVector(this Vector3 vector3)
+        {
+            return new Vector3f(vector3.x, vector3.y, vector3.z);
         }
     }
 }
